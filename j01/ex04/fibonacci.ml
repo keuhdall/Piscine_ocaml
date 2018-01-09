30788e9149eb15b5bd7e611d4b2147537498844f
keuhdall <lmarques@student.42.fr>
2018-01-09 14:51:43 +0100

    deleted useless begin and end

--- j01/ex04/fibonacci.ml
+++ j01/ex04/fibonacci.ml
@@ -2,17 +2,15 @@ let fibonacci i =
   if (i < 0) then
     -1
   else
-    begin
-      let rec loop i acc1 acc2 =
-        if (i <= 0) then
-          acc1
-        else if (i = 1) then
-          acc2
-        else
-          loop (i - 1) acc2 (acc1 + acc2)
-      in
-      loop i 0 1
-    end
+    let rec loop i acc1 acc2 =
+      if (i <= 0) then
+        acc1
+      else if (i = 1) then
+        acc2
+      else
+        loop (i - 1) acc2 (acc1 + acc2)
+    in
+    loop i 0 1
 
 let () =
   print_int (fibonacci ~-42);
