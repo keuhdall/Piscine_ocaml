25ae66f4ab17f2382f96f6a6ab1922707e45d8b4
keuhdall <lmarques@student.42.fr>
2018-01-09 17:26:06 +0100

    started working on ex08

---
+++ j01/ex08/ft_sum.ml
@@ -0,0 +1,15 @@
+let ft_sum f s e =
+  if (s > e) then
+    nan
+  else
+    let rec loop f s e acc =
+      if (s = e) then
+        acc
+      else
+        loop f (s + 1) e (acc +. (f s))
+    in
+    loop f s e 0.0
+
+let () =
+  print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10);
+  print_char '\n'
