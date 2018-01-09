145875f8ce5abba8861d42a16c0275fadb443cd1
keuhdall <lmarques@student.42.fr>
2018-01-09 14:31:07 +0100

    added j01 ex04

---
+++ j01/ex04/fibonacci.ml
@@ -0,0 +1,27 @@
+let fibonacci i =
+  if (i < 0) then
+    -1
+  else
+    begin
+      let rec loop i acc1 acc2 =
+        if (i <= 0) then
+          acc1
+        else if (i = 1) then
+          acc2
+        else
+          loop (i - 1) acc2 (acc1 + acc2)
+      in
+      loop i 0 1
+    end
+
+let () =
+  print_int (fibonacci ~-42);
+  print_char '\n';
+  print_int (fibonacci 1);
+  print_char '\n';
+  print_int (fibonacci 3);
+  print_char '\n';
+  print_int (fibonacci 6);
+  print_char '\n';
+  print_int (fibonacci 32);
+  print_char '\n'
