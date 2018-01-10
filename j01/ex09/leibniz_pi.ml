34ed360465307f974d682b1d23937c646e6fba8b
Léonard Marques <lmarques@student.42.fr>
2018-01-10 01:05:41 +0100

    still working on j01ex09

--- j01/ex09/leibniz_pi.ml
+++ j01/ex09/leibniz_pi.ml
@@ -1,27 +1,23 @@
 let leibniz_pi max =
-  if (max < 0) then
+  if (max < 0.0) then
     -1
   else
-    let ref_pi = (4.0 *. (atan 1)) in
-    let rec pow n pow =
-      if (n = 0) then
-        0
-      else
-        let rec pow_loop n pow pow_acc=
-          if (pow = 0) then
-            pow_acc
-          else
-            loop n (pow - 1) (pow_acc + (n * pow))
-    in
+    let ref_pi = (4.0 *. (atan 1.0)) in
     let rec calc_pi i acc_pi =
       if (i = ~-1) then
         (4.0 *. acc_pi)
       else
-        loop_pi (i - 1) (acc_pi +. ((float_of_int (pow -1 i)) /. (float_of_int (2 * i + 1))))
+        calc_pi (i - 1) (acc_pi +. ((-1. ** (float_of_int i)) /. float_of_int (2 * i + 1)))
     in
     let rec calc_delta j =
-      if ((calc_pi j 0.0) -. ref_pi <= float_of_int max) then
-        j
+      if ((calc_pi j 0.0) -. ref_pi >= max) then
+        begin
+          print_float (calc_pi j 0.0);
+          print_char '\n';
+          print_float ref_pi;
+          print_char '\n';
+          j
+        end
       else
         calc_delta (j + 1)
     in
