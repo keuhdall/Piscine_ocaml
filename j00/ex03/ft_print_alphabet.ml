25339cad54f6220035f772371ac6325fa1231895
Léonard Marques <lmarques@student.42.fr>
2018-01-11 01:00:32 +0100

    corrected j00 ex03

--- j00/ex03/ft_print_alphabet.ml
+++ j00/ex03/ft_print_alphabet.ml
@@ -1,7 +1,7 @@
 let ft_print_alphabet () =
   let begin_char = int_of_char 'a' in
   let rec loop curr_char =
-    if (curr_char < int_of_char 'z') then
+    if (curr_char <= int_of_char 'z') then
       begin
         print_char (char_of_int curr_char);
         loop (curr_char + 1)
