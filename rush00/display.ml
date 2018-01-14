3aa37d8395a5045d18ddf6bb31998e0e3e03cc3a
Ashbury <justasze@student.42.fr>
2018-01-14 17:15:51 +0100

    Display in progress

--- rush00/display.ml
+++ rush00/display.ml
@@ -8,10 +8,20 @@ let rec get_inner_grid grid n i = match grid with
   | [] -> invalid_arg "WTF"
   | head::tail -> if i = n then head else get_inner_grid tail n (i + 1)
 
-let print_row grid x y =
-  get_inner_grid grid.outer_content (get_grid x y) 0
+let rec get_nth_case (lst:Grid.cell_state list) n i = match lst with
+  | [] -> invalid_arg "WTF"
+  | head::tail -> if i = n then head else get_inner_grid tail n (i + 1)
+
+let print_case (case:Grid.cell_state) = match case with
+  | X -> print_char 'X'
+  | O -> print_char 'O'
+  | Pending -> print_char '_'
+
+let print_row (grid:Grid.outer_grid) x y =
+  let inner_grid = (get_inner_grid grid.outer_content (get_grid x y) 0) in
+  print_case (get_nth_case inner_grid.inner_content (get_case x y) 0)
 
 let rec print_grid grid n =
-  print_row grid.outer_content n 0
-  if n = 8 then print_string ""
+  print_row grid n 0;
+  if n = 8 then print_string "\n"
   else print_grid grid (n + 1)
--- rush00/main.ml
+++ rush00/main.ml
@@ -3,6 +3,7 @@ let blank_inner_grid = Grid.newInnerGrid [Grid.Pending;Grid.Pending;Grid.Pending
 let blank_outer_grid = Grid.newOuterGrid [blank_inner_grid;blank_inner_grid;blank_inner_grid;blank_inner_grid;blank_inner_grid;blank_inner_grid;blank_inner_grid;blank_inner_grid;blank_inner_grid] Pending
 
 let main () =
+  Display.print_grid blank_outer_grid 0;
   Reader_parser.parse blank_outer_grid grid_size
 
 let () = main ()
