148acbbb6ac2d12e18d93c00f43cef7e9fb16a04
Léonard Marques <lmarques@student.42.fr>
2018-01-19 18:56:39 +0100

    added j08 ex01

--- j08/ex01/main.ml
+++ j08/ex01/main.ml
@@ -1,19 +1,17 @@
 let () = 
-  let h =  new Atom.hydrogen in
-  let h2 =  new Atom.hydrogen in
-  let c = new Atom.carbon in
-  let o =  new Atom.oxygen in
-  let he = new Atom.helium in
-  let n =  new Atom.nitrogen in
-  let s = new Atom.sodium in
-  h#to_string () ;
-  h2#to_string () ;
-  c#to_string () ;
-  o#to_string () ;
-  he#to_string () ;
-  n#to_string () ;
-  s#to_string () ;
-  print_endline "h = h2 ?" ;
-  print_endline (string_of_bool (h#equals h2)) ;
-  print_endline "h = he ?" ;
-  print_endline (string_of_bool (h#equals he))
+  let h2o = new Molecule.water in
+  let water = new Molecule.water in
+  let co2 = new Molecule.carbon_dioxyde in
+  let serotonin = new Molecule.serotonin in
+  let coffee = new Molecule.cafein in
+  let effexor = new Molecule.venlafaxine in
+  
+  print_endline (h2o#to_string) ;
+  print_endline (co2#to_string) ;
+  print_endline (serotonin#to_string) ;
+  print_endline (coffee#to_string) ;
+  print_endline (effexor#to_string) ;
+  print_endline "is h2o = water ?" ;
+  print_endline (string_of_bool (h2o#equals water)) ;
+  print_endline "is h2o = coffee ?" ;
+  print_endline (string_of_bool (h2o#equals coffee))
---
+++ j08/ex01/molecule.ml
@@ -0,0 +1,90 @@
+class virtual molecule (name:string) (atoms:Atom.atom list) =
+  object (this)
+    method name = name
+    method atoms = atoms
+    method formula = (this#compute_formula atoms)
+
+    method private compare_atoms (a: Atom.atom) (b: Atom.atom) =
+      String.compare a#symbol b#symbol
+
+    method private hill_notation atoms =
+      let rec find_atom atoms nb (to_find: string) = match atoms with
+        | [] -> ""
+        | head::next::tail ->
+          if (head#symbol = to_find) then
+            begin
+              if (head#symbol = next#symbol) then find_atom (next::tail) (nb + 1) to_find
+              else if nb = 0 then to_find
+              else to_find ^ (string_of_int (nb + 1))
+            end
+          else find_atom (next::tail) nb to_find
+        | head::[] -> ""
+      in
+      let rec tail_hill atoms nb find_h ret = match atoms with
+        | [] -> ret
+        | head::next::tail ->
+                if (head#symbol = "C" || (find_h = false && head#symbol = "H")) then tail_hill (next::tail) nb find_h ret
+                else if (head#equals next) then tail_hill (next::tail) (nb + 1) find_h ret
+                else if (nb = 0) then tail_hill (next::tail) 0 find_h (ret ^ head#symbol)
+                else tail_hill (next::tail) 0 find_h (ret ^ head#symbol ^ (string_of_int (nb + 1)))
+        | head::[] ->
+                if (head#symbol = "C" || (find_h = false && head#symbol = "H")) then ret
+                else if (nb = 0) then ret ^ head#symbol
+                else ret ^ head#symbol ^ (string_of_int (nb + 1))
+      in
+      let carbon = find_atom atoms 0 "C" in
+      if carbon = "" then (tail_hill atoms 0 true "")
+      else carbon ^ (find_atom atoms 0 "H") ^ (tail_hill atoms 0 false "")
+
+    method private compute_formula (atoms: Atom.atom list) = match atoms with
+      | [] -> ""
+      | head::tail -> this#hill_notation (List.sort this#compare_atoms atoms)
+
+    method to_string = "Molecule of " ^ this#name ^ "(" ^ this#formula ^ ")"
+
+    method equals (that:molecule) = (this#name = that#name) && (this#formula = that#formula)
+end
+
+class water =
+  object
+    inherit molecule "Water" [new Atom.hydrogen ; new Atom.oxygen ; new Atom.hydrogen]
+end
+
+class carbon_dioxyde =
+  object
+    inherit molecule "Carbon Dioxyde" [new Atom.oxygen ; new Atom.oxygen ; new Atom.carbon]
+end
+
+class serotonin =
+  object
+    inherit molecule "Serotonin" [new Atom.oxygen ; new Atom.nitrogen ; new Atom.nitrogen ;
+    new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+    new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+    new Atom.hydrogen ; new Atom.hydrogen ;
+    new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ;
+    new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon]
+end
+
+class cafein =
+object
+  inherit molecule "Cafein" [new Atom.oxygen ; new Atom.oxygen ;
+  new Atom.nitrogen ; new Atom.nitrogen ; new Atom.nitrogen ; new Atom.nitrogen ;
+  new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ;
+  new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ;
+  new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+  new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen]
+end
+
+class venlafaxine =
+object
+  inherit molecule "Venlafaxine" [new Atom.oxygen ; new Atom.oxygen ; new Atom.nitrogen ;
+  new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ;
+  new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ;
+  new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ; new Atom.carbon ;
+  new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+  new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+  new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+  new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+  new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ; new Atom.hydrogen ;
+  new Atom.hydrogen ; new Atom.hydrogen]
+end
