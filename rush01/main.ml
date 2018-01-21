8e0837fcf7526fb92fa25d6103e50254fa5464a5
Léonard Marques <lmarques@student.42.fr>
2018-01-21 17:48:19 +0100

    finished rush01 !

--- rush01/main.ml
+++ rush01/main.ml
@@ -1,66 +1,78 @@
-open Tsdl
-open Result
-
 let mouse_click win =
-    let (_, (x, y)) = Tsdl.Sdl.get_mouse_state () in (*TODO check button*)
-    win#mouse_action (x, y)
+    let (b, (x, y)) = Tsdl.Sdl.get_mouse_state () in
+    match Int32.to_int b with
+    | 1   ->  win#mouse_action (x, y)
+    | _   ->  win
 
 let rec push_second id () =
-    let event = Sdl.Event.create () in
-    Sdl.Event.set event Sdl.Event.typ id;
-    match Sdl.push_event event (* by sguillia *) with
+    let event = Tsdl.Sdl.Event.create () in
+    Tsdl.Sdl.Event.set event Tsdl.Sdl.Event.typ id;
+    match Tsdl.Sdl.push_event event (* by sguillia *) with
     | Error (`Msg e) -> failwith (Printf.sprintf "Could push event: %s" e)
-    | Ok (_)         -> (Sdl.delay (Int32.of_int 1000); push_second id (); ())
-
+    | Ok (_)         -> (Tsdl.Sdl.delay (Int32.of_int 1000); push_second id (); ())
 
 let event_loop win =
-    match Sdl.register_event () with
+    match Tsdl.Sdl.register_event () with
     | None                      -> failwith (Printf.sprintf "Could not create event")
     | Some (eatch_second_event) ->
-        let e = Sdl.Event.create () in
-        let rec loop win = match Sdl.wait_event (Some e) with
+        let e = Tsdl.Sdl.Event.create () in
+        let rec loop win =
+            win#clear () ;
+            win#render_all () ;
+            match Tsdl.Sdl.wait_event (Some e) with
             | Error (`Msg e) -> failwith (Printf.sprintf "Could not wait event: %s" e)
             | Ok () ->
-                match Sdl.Event.(enum (get e typ)) with
+                match Tsdl.Sdl.Event.(enum (get e typ)) with
                     | `Quit                    -> ()
                     | `Mouse_button_down       -> begin loop (mouse_click win) end
                     | `User_event              -> loop (win#eatch_second ())
-                    | `Drop_file               -> begin Sdl.Event.drop_file_free e; loop win end
+                    | `Drop_file               -> begin Tsdl.Sdl.Event.drop_file_free e; loop win end
                     | _                        -> loop win in
-        Sdl.start_text_input ();
+        Tsdl.Sdl.start_text_input ();
         ignore (Thread.create (push_second eatch_second_event) ());
         loop win
 
-let start () = match Sdl.init Sdl.Init.video with
+let start () = match Tsdl.Sdl.init Tsdl.Sdl.Init.video with
     | Error (`Msg e) -> failwith (Printf.sprintf "Init error: %s" e)
     | Ok () ->
-      match Sdl.create_window ~w:800 ~h:600 "Tama" Sdl.Window.opengl with
-        | Error (`Msg e) -> Sdl.log "Create window error: %s" e; exit 1
+    match Tsdl_ttf.Ttf.init () with
+        | Error (`Msg e) -> Tsdl.Sdl.quit (); failwith (Printf.sprintf "Init error: %s" e)
+        | Ok () ->
+      match Tsdl.Sdl.create_window ~w:800 ~h:600 "Tama" Tsdl.Sdl.Window.opengl with
+        | Error (`Msg e) -> failwith (Printf.sprintf "Create window error: %s" e)
         | Ok w ->
             let tama = new Tama.tama in
             let tama = try tama#load () with
-                       | _ -> begin print_endline "Can not load file, creat new tama !"; tama end in
+                | _ -> begin print_endline "Can not load file, creat new tama !"; tama end in
             let win = new Window.window w tama in
-            let win = win#add_image "lena" "lena.bmp" in
-            let win = win#add_button Save (0, 0, 10, 10) in
-
-            let win = win#add_rectangle (Sdl.Rect.create 40 30 150 40) (0, 0, 0, 0) in
-            let win = win#add_rectangle (Sdl.Rect.create 230 30 150 40) (0, 0, 0, 0) in
-            let win = win#add_rectangle (Sdl.Rect.create 420 30 150 40) (0, 0, 0, 0) in
-            let win = win#add_rectangle (Sdl.Rect.create 610 30 150 40) (0, 0, 0, 0) in
-
-            let win = win#add_rectangle (Sdl.Rect.create 38 28 154 44) (255, 255, 255, 0) in
-            let win = win#add_rectangle (Sdl.Rect.create 228 28 154 44) (255, 255, 255, 0) in
-            let win = win#add_rectangle (Sdl.Rect.create 418 28 154 44) (255, 255, 255, 0) in
-            let win = win#add_rectangle (Sdl.Rect.create 608 28 154 44) (255, 255, 255, 0) in
-
-
+            let win = win#add_image "tama" "tama.bmp" in
+            let win = win#add_text "heal" in
+            let win = win#add_text "thunder" in
+            let win = win#add_text "bath" in
+            let win = win#add_text "kill" in
+            let win = win#add_text "save" in
+            let win = win#add_text "dead" in
+            let win = win#add_button Save (750, 550, 50, 50) in
+            let win = win#add_button Eat (38, 470, 154, 90) in
+            let win = win#add_button Thunder (228, 470, 154, 90) in
+            let win = win#add_button Bath (418, 470, 154, 90) in
+            let win = win#add_button Kill (608, 470, 154, 90) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 38 28 154 44) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 228 28 154 44) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 418 28 154 44) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 608 28 154 44) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 38 500 154 70) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 228 500 154 70) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 418 500 154 70) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 608 500 154 70) (0, 0, 0, 0) in
+            let win = win#add_rectangle (Tsdl.Sdl.Rect.create 700 400 50 50) (0, 0, 0, 0) in
             win#render_all () ;
             event_loop win;
-            Sdl.destroy_window w;
-            Sdl.quit ();
-            exit 0
+            Tsdl.Sdl.destroy_window w;
+            Tsdl_ttf.Ttf.quit ();
+            Tsdl.Sdl.quit ()
 
 let () =
   try start () with
-  | _  -> print_endline "Internal error"
+  | Failure (m)  -> print_endline m
+  | _            -> print_endline "Internal error"
--- rush01/window.ml
+++ rush01/window.ml
@@ -1,6 +1,5 @@
 type action_type = Save | Eat | Thunder | Bath | Kill
 
-open Tsdl
 class window win new_tama =
 
   object (self)
@@ -10,18 +9,32 @@ class window win new_tama =
   val button_surface : (action_type * (int * int * int * int)) list = []
   val renderer =
     begin
-      match Sdl.create_renderer win with
+      match Tsdl.Sdl.create_renderer win with
         | Error (`Msg e) -> failwith (Printf.sprintf "Error creating renderer: %s" e)
         | Ok (renderer) -> renderer
     end
+  val font =
+      begin
+          match Tsdl_ttf.Ttf.open_font "arial.ttf" 20 with
+          | Error (`Msg e) -> failwith (Printf.sprintf "Error creating font: %s" e)
+          | Ok (font) -> font
+      end
+
+ method load_text text =
+    match Tsdl_ttf.Ttf.render_text_solid font text (Tsdl.Sdl.Color.create 0 0 255 0) with
+      | Error (`Msg e) -> failwith (Printf.sprintf "Error creating font image: %s" e)
+      | Ok (surface) ->
+     match Tsdl.Sdl.create_texture_from_surface renderer surface with
+     | Error (`Msg e) -> begin Tsdl.Sdl.free_surface surface; failwith (Printf.sprintf "Error creating texture : %s" e) end
+     | Ok (texture) -> begin Tsdl.Sdl.free_surface surface; texture end
 
   method private load_image img_path =
-    match Sdl.load_bmp img_path with
+    match Tsdl.Sdl.load_bmp img_path with
       | Error (`Msg e) -> failwith (Printf.sprintf "Error loading image: %s" e)
       | Ok (surface) ->
-        match Sdl.create_texture_from_surface renderer surface with
-          | Error (`Msg e) -> begin Sdl.free_surface surface; failwith (Printf.sprintf "Error creating texture : %s" e) end
-          | Ok (texture) -> begin Sdl.free_surface surface; texture end
+        match Tsdl.Sdl.create_texture_from_surface renderer surface with
+          | Error (`Msg e) -> begin Tsdl.Sdl.free_surface surface; failwith (Printf.sprintf "Error creating texture : %s" e) end
+          | Ok (texture) -> begin Tsdl.Sdl.free_surface surface; texture end
 
   method private get_image img_name =
     let rec find images =
@@ -47,35 +60,90 @@ class window win new_tama =
 
   method add_image img_name img_path =
     {< loaded_image_tama = (img_name, self#load_image img_path)::loaded_image_tama >}
+  method add_text text =
+    {< loaded_image_tama = (text, self#load_text text)::loaded_image_tama >}
 
   method add_rectangle rect color =
     {< rectangles = (rect, color)::rectangles >}
     
   method private render_image () =
-    match Sdl.render_copy renderer (self#get_image "lena") with
+    match Tsdl.Sdl.render_copy renderer (self#get_image "tama") ~dst:(Tsdl.Sdl.Rect.create 200 100 400 400) with
       | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
-      | Ok ()           -> ()
+      | Ok () -> ()
+
+  method private render_text () =
+    match Tsdl.Sdl.set_render_draw_color renderer 0 255 0 0 with
+    | Error (`Msg e)  -> failwith (Printf.sprintf "Failed to set text color (%s)" e)
+    | Ok () ->
+      match Tsdl.Sdl.render_copy renderer (self#get_image "heal") ~dst:(Tsdl.Sdl.Rect.create 38 500 154 70) with
+        | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+        | Ok () ->
+        match Tsdl.Sdl.render_copy renderer (self#get_image "thunder") ~dst:(Tsdl.Sdl.Rect.create 228 500 154 70) with
+        | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+        | Ok () ->
+        match Tsdl.Sdl.render_copy renderer (self#get_image "bath") ~dst:(Tsdl.Sdl.Rect.create 418 500 154 70) with
+        | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+        | Ok () ->
+        match Tsdl.Sdl.render_copy renderer (self#get_image "kill") ~dst:(Tsdl.Sdl.Rect.create 608 500 154 70) with
+        | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+        | Ok () ->
+        match Tsdl.Sdl.render_copy renderer (self#get_image "save") ~dst:(Tsdl.Sdl.Rect.create 700 400 50 50) with
+        | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+        | Ok () -> ()
+      
 
   method private render_rectangles () =
     let rec render_rec rect_list =
       match rect_list with
         | []  -> ()
         | (rect, (r, g, b, a))::queue  ->
-          match Sdl.set_render_draw_color renderer r g b a with
+          match Tsdl.Sdl.set_render_draw_color renderer r g b a with
             | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
             | Ok () ->
-              match Sdl.render_fill_rect renderer (Some rect) with
+              match Tsdl.Sdl.render_fill_rect renderer (Some rect) with
                 | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
                 | Ok () -> render_rec queue in
     render_rec rectangles
 
+  method private render_gauges () =
+    let hp_gauge = int_of_float ((float_of_int tama#health) *. 1.5) in
+    let energy_gauge = int_of_float ((float_of_int tama#energy) *. 1.5) in
+    let hygiene_gauge = int_of_float ((float_of_int tama#hygiene) *. 1.5) in
+    let happyness_gauge = int_of_float ((float_of_int tama#happyness) *. 1.5) in
+    let gauges = [(Tsdl.Sdl.Rect.create 40 30 hp_gauge 40) ;
+                  (Tsdl.Sdl.Rect.create 230 30 energy_gauge 40) ;
+                  (Tsdl.Sdl.Rect.create 420 30 hygiene_gauge 40) ;
+                  (Tsdl.Sdl.Rect.create 610 30 happyness_gauge 40)] in
+    let rec render_gauges_loop g = match g with
+      | []          -> ()
+      | tete::queue ->
+        match Tsdl.Sdl.set_render_draw_color renderer 255 255 255 0 with
+          | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+          | Ok () ->
+            match Tsdl.Sdl.render_fill_rect renderer (Some tete) with
+              | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+              | Ok () -> render_gauges_loop queue in
+      render_gauges_loop gauges
+
   method render_all () =
-    match Sdl.render_clear renderer with
+    match Tsdl.Sdl.render_clear renderer with
       | Error (`Msg e)  ->  failwith (Printf.sprintf "Window.window.render_all : Failed cleaning window (%s)" e)
       | Ok ()           ->
           self#render_image () ;
-          self#render_rectangles ();
-          Sdl.render_present renderer
+          self#render_rectangles () ;
+          self#render_gauges () ;
+          self#render_text () ;
+          if tama#is_live () = false
+          then
+            begin
+              match Tsdl.Sdl.set_render_draw_color renderer 0 0 0 0 with
+              | Error (`Msg e)  -> failwith (Printf.sprintf "Failed to set text color (%s)" e)
+              | Ok () ->
+              match Tsdl.Sdl.render_copy renderer (self#get_image "dead") ~dst:(Tsdl.Sdl.Rect.create 0 0 800 600) with
+                | Error (`Msg e)  -> failwith (Printf.sprintf "Window.window.render_image : failed to render image (%s)" e)
+                | Ok () -> ();
+            end;
+            Tsdl.Sdl.render_present renderer
 
   method mouse_action (click_x, click_y) =
       {< tama = match self#get_button (click_x, click_y) with
@@ -88,5 +156,13 @@ class window win new_tama =
 
   method eatch_second () =
       {< tama = (tama#second_passe ()) >}
+
+  method clear () =
+    match Tsdl.Sdl.set_render_draw_color renderer 255 255 255 0 with
+      | Error (`Msg e)  -> failwith (Printf.sprintf "Failed to set render color (%s)" e)
+      | Ok () ->
+        match Tsdl.Sdl.render_clear renderer with
+          | Error (`Msg e) -> failwith (Printf.sprintf "Failed to clear window : %s" e)
+          | Ok () -> ()
 
 end
